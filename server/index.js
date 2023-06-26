3a906b4e3e3f4bf8e03181d49d13ecb699039548
Ievarep236 <54540748+Ievarep236@users.noreply.github.com>
2023-06-27 01:11:59 +0300

    Groups backend done

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 const register = require("./src/routes/register");
 const login = require("./src/routes/login");
+const groups = require("./src/routes/groups");
 
 const server = express();
 
@@ -11,6 +12,7 @@ server.use(express.json());
 
 server.use("/register", register);
 server.use("/login", login);
+server.use("/groups", groups);
 const PORT = process.env.PORT;
 
 server.listen(8080, () => console.log(`server is running on ${PORT} `));
